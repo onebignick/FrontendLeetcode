8c2c9a4bc9f36105f36a1e182ef9a208b5dbd932
Qingyu255 <qingyu2555@gmail.com>
2024-05-13 00:41:15 +0800

    set up next app

---
+++ frontendracers/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="test-2xl font-bold p-10">FRONTEND RACERS</h1>
+    </main>
+  );
+}
