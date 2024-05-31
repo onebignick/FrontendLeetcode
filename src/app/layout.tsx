1490c960d1b3dd121158581eff84fdc11f2437c8
0Ogw <glen.wang2709@gmail.com>
2024-05-31 20:04:26 +0800

    update logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Frontend Racers",
-  description: "Come sharpen your Frontend Development skills with Frontend racers",
-  icons: {
-    icon: "logo.png"
-  }
+	title: 'Frontend Racers',
+	description:
+		'Come sharpen your Frontend Development skills with Frontend racers',
+	icons: {
+		icon: 'logo.png',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Navbar/>
-          <div className="min-h-screen">
-            {children}
-          </div>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className}`}>
+				<Navbar />
+				<div className="min-h-screen">{children}</div>
+				<Footer />
+			</body>
+		</html>
+	);
 }
