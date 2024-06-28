e4d0ec339676693e9132b6b39856d16acbc64d74
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-06-28 23:37:42 +0800

    secure webhook

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,11 +1,41 @@
 import { UserRepository } from "@/lib/repository/user/UserRepository";
 import type { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+	const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 	const userRepository: UserRepository = new UserRepository();
+
+	if (!WEBHOOK_SECRET) {
+		throw new Error("CLERK_WEBHOOK_SECRET not found");
+	}
+
+	const headerPayload = headers();
+	const svix_id = headerPayload.get("svix-id");
+	const svix_timestamp = headerPayload.get("svix-timestamp");
+	const svix_signature = headerPayload.get("svix-signature");
+
+	if (!svix_id || !svix_timestamp || !svix_signature) {
+		return new Response("No svix headers found", {
+			status: 400
+		});
+	}
+
 	try {
-		const event = (await request.json()) as WebhookEvent;
+		const payload = await request.json();
+		const body = JSON.stringify(payload);
+
+		const wh = new Webhook(WEBHOOK_SECRET);
+		let event: WebhookEvent;
+
+		event = wh.verify(body, {
+			"svix-id": svix_id,
+			"svix-timestamp": svix_timestamp,
+			"svix-signature": svix_signature,
+		}) as WebhookEvent;
+
 		const { id: clerkUserId } = event.data;
 		if (!clerkUserId) {
 			return NextResponse.json(
