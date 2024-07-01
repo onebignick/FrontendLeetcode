209f9309711186beaecf48fcb1ec3f283f6d423b
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-07-01 15:00:43 +0800

    update contribute page

---
+++ src/app/contribute/page.tsx
@@ -0,0 +1,7 @@
+export default function Contribute() {
+	return (
+		<main>
+			Hello world this is the Contribute page
+		</main>
+	);
+}
--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -2,6 +2,7 @@ import { QuestionRepository } from "@/lib/repository/question/QuestionRepository
 import QuestionCard from "../../components/questions/QuestionCard";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Alert, AlertTitle } from "@/components/ui/alert";
 
 // export const dynamic = "force-dynamic";
 
@@ -10,11 +11,16 @@ export default async function QuestionPage() {
     const questions = await questionRepository.getAll();
 
     return (
-        <>
-            <Button>
-                <Link href="/contribute">Contribute</Link>
-            </Button>
+        <div className="flex flex-col gap-4">
+            <Alert>
+                <AlertTitle className="flex justify-between items-center">
+                    Want to contribute a question?
+                    <Button>
+                        <Link href="/contribute">Contribute Question</Link>
+                    </Button>
+                </AlertTitle>
+            </Alert>
             {questions?.map((question: any, index: number) => (<QuestionCard key={question.id} question={question} index={index + 1} />))}
-        </>
+        </div>
     )
 }
