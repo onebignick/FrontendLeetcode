a3ba1fc9fbb2d5390e81f503f909e878546059e5
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-07-03 00:09:17 +0800

    fix build errors

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default async function AdminDashboard(params: { searchParams: { search?: 
 	return (
 		<>
 			<h1>This is the admin dashboard</h1>
-			<p>This page is restricted to users with the 'admin' role.</p>
+			<p>This page is restricted to users with the admin role.</p>
 
 			<SearchUsers />
 
