e6b41dc18de0ff28dfd388b7e048e4f6231120a0
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-07-25 11:48:30 +0800

    remove unneeded components

--- src/app/_components/adminPage.tsx
+++ src/app/_components/adminPage.tsx
@@ -6,9 +6,9 @@ export default function AdminPage() {
 
     return(
         <main className="px-5 lg:px-10 xl:px-15 py-5">
-            <div className="flex flex-row space-x-24 w-full">
+            <div className="flex flex-row space-x-12 w-full">
                 <div className="basis-2/3"><VisitorChart/></div>
-                <div className="basis-1/3 flex flex-col space-y-16">
+                <div className="basis-1/3 flex flex-col space-y-12">
                     <ApproveQuestions/>
                     <RecentSignups/>
                 </div>
